450e5ed1a7aa01f77fca8826d1b035d61c7fdda7
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-03-18 14:36:12 -0600

    Run training on CPU.

--- tests/test_eda.py
+++ tests/test_eda.py
@@ -5,14 +5,24 @@ from pytorch_retrieve.config import InputConfig, OutputConfig
 from pytorch_retrieve.eda import run_eda
 
 
-def test_eda(tmp_path, mlp_training_schedule):
+def test_eda(
+        tmp_path,
+        mlp_training_schedule,
+        cpu_compute_config,
+):
     """
     Ensure that running EDA produces the expected artifacts.
     """
     input_configs = {"x": InputConfig(n_features=1)}
     output_configs = {"y": OutputConfig(target="y", kind="Mean", shape=1)}
     training_config = next(iter(mlp_training_schedule.values()))
-    run_eda(tmp_path / "stats", input_configs, output_configs, training_config)
+    run_eda(
+        tmp_path / "stats",
+        input_configs,
+        output_configs,
+        training_config,
+        compute_config=cpu_compute_config
+    )
 
     assert (tmp_path / "stats" / "input" / "x.nc").exists()
     assert (tmp_path / "stats" / "output" / "y.nc").exists()
