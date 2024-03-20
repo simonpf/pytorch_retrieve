8571c209a1bd7dd84564336656a32084ce9ad139
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-03-20 14:15:10 -0600

    Add tensors to represent classification outputs.

---
+++ pytorch_retrieve/tensors/classification.py
@@ -0,0 +1,171 @@
+"""
+pytorch_retrieve.tensors.classification
+=======================================
+
+Provides the 'DetectionTensor' and 'ClassificationTensor' classes representing
+the ouput from detection (binary classification) and mulit-class
+ classification.
+"""
+import torch
+from torch import nn
+
+from .masked_tensor import MaskedTensor
+
+
+class DetectionTensor(torch.Tensor):
+    """
+    A DetectionTensor is a tensor that holds predictions of a detection
+    of two-class classification tasks.
+    """
+    def __new__(cls, *args, **kwargs):
+        tensor = super().__new__(cls, *args, **kwargs)
+
+        # Keep reference to original tensor.
+        if isinstance(args[0], DetectionTensor):
+            tensor.base = args[0].base
+        else:
+            tensor.base = args[0]
+
+        return tensor
+
+    @classmethod
+    def get_base(cls, arg) -> torch.Tensor:
+        """
+        Get 'base' tensor of an argument.
+
+        Return the raw data tensors from an argument that is a ProbabilityTensor
+        or an iterable containing probability tensors.
+        """
+        if isinstance(arg, tuple):
+            return tuple([cls.get_base(elem) for elem in arg])
+        elif isinstance(arg, list):
+            return [cls.get_base(elem) for elem in arg]
+        elif isinstance(arg, dict):
+            return {key: cls.get_base(value) for key, value in arg.items()}
+        elif isinstance(arg, cls):
+            return arg.base
+        return arg
+
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        """ """
+        if kwargs is None:
+            kwargs = {}
+        args = [cls.get_base(arg) for arg in args]
+        kwargs = {key: cls.get_base(val) for key, val in kwargs.items()}
+        result = func(*args, **kwargs)
+
+        if func == torch.as_tensor:
+            return result
+
+        if func == torch.Tensor.unbind or func == torch.unbind:
+            return tuple([DetectionTensor(tensor) for tensor in result])
+
+        if isinstance(result, torch.Tensor):
+            return DetectionTensor(result)
+        return result
+
+    def __repr__(self):
+        tensor_repr = self.base.__repr__()
+        return "DetectionTensor" + tensor_repr[6:]
+
+    def loss(self, y_true: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            y_true: Tensor containing the true values.
+
+        Return:
+            The binary cross-entropy loss of this tensor and the given
+            true label.
+
+        """
+        return nn.functional.binary_cross_entropy_with_logits(
+            self.base,
+            y_true,
+        )
+
+    def probability(self):
+        """
+        Return detection probabilities.
+        """
+        return torch.sigmoid(self.base)
+
+
+class ClassificationTensor(torch.Tensor):
+    """
+    A classification tensor is a tensor that holds predictions of a classification
+    task.
+    """
+
+    @classmethod
+    def get_base(cls, arg) -> torch.Tensor:
+        """
+        Get 'base' tensor of an argument.
+
+        Return the raw data tensors from an argument that is a ProbabilityTensor
+        or an iterable containing probability tensors.
+        """
+        if isinstance(arg, tuple):
+            return tuple([cls.get_base(elem) for elem in arg])
+        elif isinstance(arg, list):
+            return [cls.get_base(elem) for elem in arg]
+        elif isinstance(arg, dict):
+            return {key: cls.get_base(value) for key, value in arg.items()}
+        elif isinstance(arg, cls):
+            return arg.base
+        return arg
+
+    def __new__(cls, *args, **kwargs):
+        tensor = super().__new__(cls, *args, **kwargs)
+
+        # Keep reference to original tensor.
+        if isinstance(args[0], ClassificationTensor):
+            tensor.base = args[0].base
+        else:
+            tensor.base = args[0]
+
+        return tensor
+
+    @classmethod
+    def __torch_function__(cls, func, types, args=(), kwargs=None):
+        """ """
+        if kwargs is None:
+            kwargs = {}
+        args = [cls.get_base(arg) for arg in args]
+        kwargs = {key: cls.get_base(val) for key, val in kwargs.items()}
+        result = func(*args, **kwargs)
+
+        if func == torch.as_tensor:
+            return result
+
+        if func == torch.Tensor.unbind or func == torch.unbind:
+            return tuple([ClassificationTensor(tensor) for tensor in result])
+
+        if isinstance(result, torch.Tensor):
+            return ClassificationTensor(result)
+        return result
+
+    def __repr__(self):
+        tensor_repr = self.base.__repr__()
+        return "ClassificationTensor" + tensor_repr[6:]
+
+    def loss(self, y_true: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            y_true: Tensor containing the true values.
+
+        Return:
+            The binary cross-entropy loss of this tensor and the given
+            true label.
+
+        """
+        return nn.functional.cross_entropy(
+            self.base,
+            y_true,
+        )
+
+    def probability(self) -> torch.Tensor:
+        """
+        Return class probabilities.
+        """
+        return torch.softmax(self.base, 1)
---
+++ tests/tensors/test_classification.py
@@ -0,0 +1,49 @@
+"""
+Tests for the 'pytorch_retrieve.tensors.classification' module.
+"""
+import torch
+
+from pytorch_retrieve.tensors.masked_tensor import MaskedTensor
+from pytorch_retrieve.tensors.classification import (
+    DetectionTensor,
+    ClassificationTensor
+)
+
+def test_detection_tensor():
+    """
+    Ensure that masked values are ignored in calculation of masked binary
+    cross entropy.
+    """
+    tensor = DetectionTensor(100 * torch.rand(10, 10, 10))
+    mask = torch.rand(10, 10, 10) - 0.5 > 0
+    target = torch.ones_like(tensor.base)
+    target[mask] = 0
+    target = MaskedTensor(target, mask=mask)
+    loss = tensor.loss(target)
+    loss_masked = tensor[~mask].loss(target[~mask])
+    assert torch.isclose(loss, loss_masked)
+
+    probs = tensor.probability()
+    assert (probs >= 0.0).all()
+    assert (probs <= 1.0).all()
+
+
+def test_classification_tensor():
+    """
+    Ensure that masked values are ignored in calculation of masked binary
+    cross entropy.
+    """
+    tensor = ClassificationTensor(100 * torch.rand(10, 20, 30))
+
+    target = torch.ones(10, 30).to(dtype=torch.int64)
+    mask = torch.rand(10, 30) - 0.5 > 0
+    target[mask] = 0
+    target = MaskedTensor(target, mask=mask)
+
+    loss = tensor.loss(target)
+    loss_masked = tensor.transpose(1, -1)[~mask].loss(target[~mask])
+    assert torch.isclose(loss, loss_masked)
+
+    probs = tensor.probability()
+    assert (probs >= 0.0).all()
+    assert (probs <= 1.0).all()
