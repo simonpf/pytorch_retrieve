1c523ad324d00b17f4f39346db8922c445c80b43
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-04-12 08:43:15 -0600

    Extend metrics to support calculation of conditional metrics.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -8,7 +8,12 @@ except ImportError:
 import pytest
 import torch
 
-from pytorch_retrieve.metrics import Bias, CorrelationCoef, PlotSamples
+from pytorch_retrieve.metrics import (
+    Bias,
+    CorrelationCoef,
+    MSE,
+    PlotSamples,
+)
 from pytorch_retrieve.tensors import MaskedTensor, MeanTensor
 
 
@@ -27,6 +32,35 @@ def test_bias():
 
     result = bias.compute()
     assert torch.isclose(result, torch.tensor(0.0))
+
+    bias = Bias(conditional={"rand": (0, 1, 4)})
+    data_loader = data_loader_1d(1024, 128)
+
+    # Test conditional bias with regularly-spaced bins.
+    for x, y in data_loader:
+        mask = torch.rand(y.shape) > 0.5
+        y[mask] = torch.nan
+        y = MaskedTensor(y, mask=mask)
+        rand = torch.rand(y.shape)
+        bias.update(y, y, conditional={"rand": rand})
+
+    result = bias.compute()
+    assert result.shape == (4,)
+    assert torch.isclose(result, torch.tensor(0.0)).all()
+
+    # Test conditional bias with fixed number of bins
+    bias = Bias(conditional={"rand": 4})
+    data_loader = data_loader_1d(1024, 128)
+    for x, y in data_loader:
+        mask = torch.rand(y.shape) > 0.5
+        y[mask] = torch.nan
+        y = MaskedTensor(y, mask=mask)
+        rand = torch.rand(y.shape) * 4
+        bias.update(y, y, conditional={"rand": rand})
+    result = bias.compute()
+    assert result.shape == (4,)
+    assert torch.isclose(result, torch.tensor(0.0)).all()
+
 
 
 def test_correlation_coef():
@@ -54,6 +88,51 @@ def test_correlation_coef():
 
     result = corr.compute()
     assert torch.isclose(result, torch.tensor(-1.0))
+
+    # Test conditional bias with regularly-spaced bins.
+    corr = CorrelationCoef(conditional={"rand": (0, 1, 4)})
+    for x, y in data_loader:
+        mask = torch.rand(y.shape) > 0.5
+        y[mask] = torch.nan
+        y = MaskedTensor(y, mask=mask)
+        rand = torch.rand(y.shape)
+        corr.update(y, y, conditional={"rand": rand})
+
+    result = corr.compute()
+    assert result.shape == (4,)
+    assert torch.isclose(result, torch.tensor(1.0)).all()
+
+
+def test_mse():
+    """
+    Test that calculating the MSE works with masked tensors.
+    """
+    mse = MSE()
+    data_loader = data_loader_1d(10 * 1024, 128)
+
+    for x, y in data_loader:
+        mask = torch.rand(y.shape) > 0.5
+        y.set_(torch.normal(torch.zeros_like(y), torch.ones_like(y)))
+        y[mask] = torch.nan
+        y = MaskedTensor(y, mask=mask)
+        mse.update(y, torch.zeros_like(y))
+
+    result = mse.compute()
+    assert torch.isclose(result, torch.tensor(1.0), atol=0.10)
+
+    # Test conditional bias with regularly-spaced bins.
+    mse = MSE(conditional={"rand": (0, 1, 4)})
+    for x, y in data_loader:
+        mask = torch.rand(y.shape) > 0.5
+        y.set_(torch.normal(torch.zeros_like(y), torch.ones_like(y)))
+        y[mask] = torch.nan
+        y = MaskedTensor(y, mask=mask)
+        rand = torch.rand(y.shape)
+        mse.update(y, torch.zeros_like(y), conditional={"rand": rand})
+
+    result = mse.compute()
+    assert result.shape == (4,)
+    assert torch.isclose(result, torch.tensor(1.0), atol=0.10).all()
 
 
 @pytest.mark.skipif(not HAS_MATPLOTLIB, reason="needs matplotlib")
