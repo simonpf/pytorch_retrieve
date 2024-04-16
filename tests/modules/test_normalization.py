df3833f17db9ae16dea345b58dda5d9034f92e81
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2024-04-16 06:50:22 -0600

    Add RMS norm layer.

--- tests/modules/test_normalization.py
+++ tests/modules/test_normalization.py
@@ -9,10 +9,47 @@ from pytorch_retrieve.modules import normalization
 
 def test_layer_norm_first():
     """
-    Retrieve LayerNormFirst module by name and ensure that tensor can be
-    forwarded through it.
+    Retrieve LayerNormFirst module by name and ensure that:
+    - Tensors can be propagated through it
+    - The dtype is conserved
+    - Normalization works for 5D inputs.
     """
     normalization_factory = normalization.get_normalization_factory("LayerNormFirst")
     norm = normalization_factory(32)
-    x = torch.rand(1, 32, 32, 32)
+
+    x = torch.rand(1, 32, 32, 32).to(dtype=torch.float32)
     y = norm(x)
+    assert y.dtype == torch.float32
+
+    norm = norm.to(dtype=torch.bfloat16)
+    x = torch.rand(1, 32, 32, 32).to(dtype=torch.bfloat16)
+    y = norm(x)
+    assert y.dtype == torch.bfloat16
+
+    x = torch.rand(1, 32, 16, 32, 32).to(dtype=torch.bfloat16)
+    y = norm(x)
+    assert y.dtype == torch.bfloat16
+
+
+def test_rms_norm():
+    """
+    Retrieve RMSNorm module by name and ensure that:
+    - Tensors can be propagated through it
+    - The dtype is conserved
+    - Normalization works for 5D inputs.
+    """
+    normalization_factory = normalization.get_normalization_factory("RMSNorm")
+    norm = normalization_factory(32)
+
+    x = torch.rand(1, 32, 32, 32).to(dtype=torch.float32)
+    y = norm(x)
+    assert y.dtype == torch.float32
+
+    norm = norm.to(dtype=torch.bfloat16)
+    x = torch.rand(1, 32, 32, 32).to(dtype=torch.bfloat16)
+    y = norm(x)
+    assert y.dtype == torch.bfloat16
+
+    x = torch.rand(1, 32, 16, 32, 32).to(dtype=torch.bfloat16)
+    y = norm(x)
+    assert y.dtype == torch.bfloat16
